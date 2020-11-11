cec4d86e3228a2e1f631c13ad05218226b95b6d9
Temidayo99 <oyelamitemidayo99@gmail.com>
2020-11-11 08:00:53 +0100

    added files

---
+++ contact-page/contact.js
@@ -0,0 +1,10 @@
+//function to open the hamburger menu
+function openNav() {
+    document.getElementById("myLinks").style.width = "100%";
+  }
+  
+  //function to close the hamburger menu
+  function closeNav() {
+    document.getElementById("myLinks").style.width = "0";
+  }
+  
